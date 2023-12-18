e9ad54978653eb7e6b2370851c345708290fd194
Lorenzo Flores (Lj) <47231323+ljyflores@users.noreply.github.com>
2023-12-17 22:26:36 -0500

    Add node js bq queries

--- query.js
+++ query.js
@@ -15,41 +15,42 @@
 'use strict';
 
 function main() {
-  // [START bigquery_query]
-  // [START bigquery_client_default_credentials]
-  // Import the Google Cloud client library using default credentials
-  const {BigQuery} = require('@google-cloud/bigquery');
-  const bigquery = new BigQuery();
-  // [END bigquery_client_default_credentials]
-  async function query() {
-    // Queries the U.S. given names dataset for the state of Texas.
+    // [START bigquery_query]
+    // [START bigquery_client_default_credentials]
+    // Import the Google Cloud client library using default credentials
+    const {BigQuery} = require('@google-cloud/bigquery');
+    const bigquery = new BigQuery();
+    // [END bigquery_client_default_credentials]
+    async function query() {
 
-    const query = `SELECT agg.table.* 
-    FROM (SELECT stop_id, ARRAY_AGG(STRUCT(table) 
-    ORDER BY insertion_time DESC)[SAFE_OFFSET(0)] agg 
-    FROM \`eco-folder-402813.jeep_etas.test\` table 
-    GROUP BY stop_id)`;
+        const query = `SELECT agg.table.* 
+        FROM (SELECT stop_id, ARRAY_AGG(STRUCT(table) 
+        ORDER BY insertion_time DESC)[SAFE_OFFSET(0)] agg 
+        FROM \`eco-folder-402813.jeep_etas.test\` table 
+        GROUP BY stop_id)`;
 
-    // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
-    const options = {
-      query: query,
-      // Location must match that of the dataset(s) referenced in the query.
-      location: 'US',
-      projectId: 'eco-folder-402813'
-    };
+        // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+        const options = {
+            query: query,
+            // Location must match that of the dataset(s) referenced in the query.
+            location: 'US',
+            projectId: 'eco-folder-402813'
+        };
 
-    // Run the query as a job
-    const [job] = await bigquery.createQueryJob(options);
-    console.log(`Job ${job.id} started.`);
+        // Run the query as a job
+        const [job] = await bigquery.createQueryJob(options);
+        console.log(`Job ${job.id} started.`);
 
-    // Wait for the query to finish
-    const [rows] = await job.getQueryResults();
+        // Wait for the query to finish
+        const [rows] = await job.getQueryResults();
 
-    // Print the results
-    console.log('Rows:');
-    rows.forEach(row => console.log(row));
-  }
-  // [END bigquery_query]
-  query();
+        // Print the results
+        // console.log('Rows:');
+        // rows.forEach(row => console.log(row));
+        console.log(`Job ${job.id} finished.`)
+        return rows
+    }
+    // [END bigquery_query]
+    query();
 }
 main();
